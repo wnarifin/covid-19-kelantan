a41aaf33cecce195be4f1afd293bbc8df9e57eaa
wnarifin <wnarifin@yahoo.com>
2021-05-30 21:03:51 +0800

    updates
    Signed-off-by: wnarifin <wnarifin@yahoo.com>

--- covid-19_kelantan_fb.R
+++ covid-19_kelantan_fb.R
@@ -17,6 +17,7 @@ library(stringr)
 # https://web.facebook.com/page/711798172246954/search/?q=RINGKASAN%20SITUASI%20TERKINI%20COVID-19
 
 # jkn_url = ""; my_date = "2021-05-"
+# jkn_url = "https://www.facebook.com/HealthofKelantan/posts/4080106208749450"; my_date = "2021-05-30"
 # jkn_url = "https://m.facebook.com/HealthofKelantan/posts/4077089929051078"; my_date = "2021-05-29"
 # jkn_url = "https://www.facebook.com/HealthofKelantan/posts/4074073062686098"; my_date = "2021-05-28"
 # jkn_url = "https://www.facebook.com/HealthofKelantan/posts/4070791336347604"; my_date = "2021-05-27"
